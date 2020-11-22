8feb20df916e226be5990e6a827ca791d7354701
Felipe Ribeiro <piporibeiro@gmail.com>
2020-11-22 14:55:00 -0300

    feat: create repo.module e repo.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import * as ormOptions from './config/orm';
+import RepoModule from './repo.module';
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormOptions), RepoModule],
   controllers: [AppController],
   providers: [AppService],
 })
